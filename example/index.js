f17106c8f67106776a716d36646af7157f1406f6
zhaohongyang1 <zhaohongyang1@jd.com>
2018-04-23 14:30:40 +0800

    feat:add 404

--- example/index.js
+++ example/index.js
@@ -5,10 +5,17 @@ import {Route, Switch,} from "react-router";
 import {HashRouter as Router} from "react-router-dom";
 import routes from "./routes/index";
 
+const NotFound = () =>(
+  <div>
+    Not Found
+  </div>
+)
+
 render(
   <Router>
     <Switch>
       {routes.map((route, i) => <Route key={i} exact={!!route.exact} path={route.path} component={route.component}/>)}
+      <Route component={NotFound}/>
     </Switch>
   </Router>, document.getElementById('app')
 );
