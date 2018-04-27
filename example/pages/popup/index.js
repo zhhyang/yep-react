04fdca546a594cc21845e6c798b8c0272935120f
zhaohongyang1 <zhaohongyang1@jd.com>
2018-04-27 10:15:07 +0800

    feat:Popup组件添加示例

--- example/pages/popup/index.js
+++ example/pages/popup/index.js
@@ -8,8 +8,12 @@ export default class Demo extends PureComponent {
   constructor() {
     super()
     this.handleToggleShow = this.handleToggleShow.bind(this)
+    this.handleToggleSlideDownShow = this.handleToggleSlideDownShow.bind(this)
+    this.handleToggleSlideRightShow = this.handleToggleSlideRightShow.bind(this)
     this.state = {
-      show: false
+      show: false,
+      slideDownShow:false,
+      slideRightShow:false,
     }
   }
 
@@ -19,11 +23,27 @@ export default class Demo extends PureComponent {
     })
   }
 
+  handleToggleSlideDownShow() {
+    this.setState({
+      slideDownShow: !this.state.slideDownShow
+    })
+  }
+  handleToggleSlideRightShow() {
+    this.setState({
+      slideRightShow: !this.state.slideRightShow
+    })
+  }
+
+
   render() {
     return (
       <Page title="Popup">
         <WhiteSpace/>
         <Button type="primary" onClick={this.handleToggleShow}>显示</Button>
+        <WhiteSpace/>
+        <Button onClick={this.handleToggleSlideDownShow}>从上向下显示</Button>
+        <WhiteSpace/>
+        <Button type="primary" onClick={this.handleToggleSlideRightShow}>从右向左显示</Button>
         <WhiteSpace/>
         <Popup show={this.state.show} onCancel={this.handleToggleShow}>
           <div className="ul">
@@ -32,7 +52,17 @@ export default class Demo extends PureComponent {
             <div className="" onClick={() => {alert('离异')}}>离异</div>
           </div>
         </Popup>
-        <WhiteSpace/>
+
+        <Popup show={this.state.slideDownShow} onCancel={this.handleToggleSlideDownShow} popupTransition="slide-down">
+          <div className="up">
+            <div className="cur" onClick={() => {alert('未婚')}}>未婚</div>
+          </div>
+        </Popup>
+        <Popup show={this.state.slideRightShow} onCancel={this.handleToggleSlideRightShow} popupTransition="slide-right">
+          <div className="right">
+            <div className="cur" onClick={() => {alert('未婚')}}>未婚</div>
+          </div>
+        </Popup>
       </Page>
     );
   }
