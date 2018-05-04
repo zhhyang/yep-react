e7e42667bf272726df5daf6eec10240c135f6494
zhaohongyang1 <zhaohongyang1@jd.com>
2018-05-04 09:08:50 +0800

    fix:删除无用代码

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-export { default as HelloWorld} from './hello-world';
 export { default as Switch} from './switch';
 export { default as Checkbox} from './checkbox';
 export { default as Radio} from './radio';
