7b82cd0647d626a78dbbf2698553737aaa7f19c2
lujingjing9 <lujingjing1@jd.com>
2018-05-21 09:59:29 +0800

    回到顶部

--- src/to-top/index.jsx
+++ src/to-top/index.jsx
@@ -1,29 +1,29 @@
-import React , { PureComponent } from 'react';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import classNames from 'classnames';
-import { Gotop } from '@jdcfe/lrc';
+import {Gotop} from '@jdcfe/lrc';
 
-export default class ToTop extends PureComponent{
+export default class ToTop extends PureComponent {
   static propTypes = {
     prefixCls: PropTypes.string,
     className: PropTypes.string,
     style: PropTypes.object,
-  }
-  
+  };
+
   static defaultProps = {
     prefixCls: 'Yep-to-top',
     style: {},
+  };
+
+  constructor() {
+    super();
   }
-  
-  constructor(){
-    super()
-  }
-  
-  render(){
+
+  render() {
     const {className, onClick, smooth, children, displayHeight, duration} = this.props;
-    const cls = className ?  className : `Yep-to-top`;
-    
+    const cls = className ? className : `Yep-to-top`;
+
     return (
       <div>
         <p>滚动至页面底部，查看右下角返回顶部按钮</p>
@@ -37,7 +37,34 @@ export default class ToTop extends PureComponent{
         <p>滚动至页面底部，查看右下角返回顶部按钮</p>
         <p>滚动至页面底部，查看右下角返回顶部按钮</p>
         <p>滚动至页面底部，查看右下角返回顶部按钮</p>
-        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
         <p>滚动至页面底部，查看右下角返回顶部按钮</p>
         <p>滚动至页面底部，查看右下角返回顶部按钮</p>
         <p>滚动至页面底部，查看右下角返回顶部按钮</p>
@@ -48,7 +75,34 @@ export default class ToTop extends PureComponent{
         <p>滚动至页面底部，查看右下角返回顶部按钮</p>
         <p>滚动至页面底部，查看右下角返回顶部按钮</p>
         <p>滚动至页面底部，查看右下角返回顶部按钮</p>
-        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
         <p>滚动至页面底部，查看右下角返回顶部按钮</p>
         <p>滚动至页面底部，查看右下角返回顶部按钮</p>
         <p>滚动至页面底部，查看右下角返回顶部按钮</p>
@@ -64,7 +118,34 @@ export default class ToTop extends PureComponent{
         <p>滚动至页面底部，查看右下角返回顶部按钮</p>
         <p>滚动至页面底部，查看右下角返回顶部按钮</p>
         <p>滚动至页面底部，查看右下角返回顶部按钮</p>
-        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
         <p>滚动至页面底部，查看右下角返回顶部按钮</p>
         <p>滚动至页面底部，查看右下角返回顶部按钮</p>
         <p>滚动至页面底部，查看右下角返回顶部按钮</p>
@@ -90,9 +171,8 @@ export default class ToTop extends PureComponent{
           children={children}
           displayHeight={displayHeight}
           duration={duration}
-        >
-        </Gotop>
+        />
       </div>
-    )
+    );
   }
 }
