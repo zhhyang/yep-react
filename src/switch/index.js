c9069a9408b2143154efa8856c603d85b37d2668
zhaohongyang1 <zhaohongyang1@jd.com>
2018-10-08 19:24:24 +0800

    修复switch组件的bug

--- src/switch/index.js
+++ src/switch/index.js
@@ -8,50 +8,73 @@ export default class Switch extends PureComponent {
     style: PropTypes.object,
     currentStatus: PropTypes.bool,
     handleSwitch: PropTypes.func,
+    onClick: PropTypes.func,
     disabled: PropTypes.bool,
+    name: PropTypes.string,
+    color: PropTypes.string,
   };
 
   static defaultProps = {
     prefixCls: 'Yep-switch',
     style: {},
+    name: '',
     currentStatus: false,
     disabled: false,
+    onClick: () => {},
     handleSwitch: () => {},
   };
 
   constructor(props) {
     super(props);
-    this.handleSwitch = this.handleSwitch.bind(this);
-    this.state = {
-      switchOn: props.currentStatus,
-    };
+    this.onClick = this.onClick.bind(this);
+    this.onChange = this.onChange.bind(this);
   }
 
-  handleSwitch() {
-    const {handleSwitch} = this.props;
-    const nextSwitchOn = !this.state.switchOn;
-    this.setState({
-      switchOn: nextSwitchOn,
-    });
-    handleSwitch(nextSwitchOn);
+  onChange(e) {
+    const {checked} = e.target;
+    if (this.props.handleSwitch) {
+      this.props.handleSwitch(checked);
+    }
+  }
+
+  onClick(e) {
+    const {onClick, currentStatus: checked} = this.props;
+    if (onClick) {
+      let val;
+      if (e && e.target && e.target.checked !== undefined) {
+        val = e.target.checked;
+      } else {
+        val = checked;
+      }
+      onClick(val);
+    }
   }
 
   render() {
-    const {disabled, prefixCls, className, style} = this.props;
-    const wrapperCls = classNames(`${prefixCls}-wrapper`, className, {
-      open: this.state.switchOn,
-      close: !this.state.switchOn,
-      disabled: disabled,
-    });
-    const innerCls = classNames('switch-inner', {
-      open: this.state.switchOn,
-      close: !this.state.switchOn,
+    const {disabled, prefixCls, className, style, color, name, currentStatus: checked} = this.props;
+    const wrapperCls = classNames(prefixCls, className);
+    const innerCls = classNames('checkbox', {
+      [`checkbox-disabled`]: disabled,
     });
 
+    if (color && checked) {
+      style.backgroundColor = color;
+    }
+
     return (
-      <div className={wrapperCls} style={style} {...(!disabled ? {onClick: this.handleSwitch} : {})}>
-        <div className={innerCls} />
-      </div>
+      <label className={wrapperCls}>
+        <input
+          type="checkbox"
+          name={name}
+          className={`${prefixCls}-checkbox`}
+          disabled={disabled}
+          checked={checked}
+          onChange={this.onChange}
+          value={checked ? 'on' : 'off'}
+          {...(!disabled ? {onClick: this.handleSwitch} : {})}
+        />
+        <div className={innerCls} style={style} {...(disabled ? {onClick: this.handleSwitch} : {})} />
+      </label>
     );
   }
 }
--- src/tabs/Tabs.jsx
+++ src/tabs/Tabs.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import noop from '../_utils/noop';
 import Gesture from '../gesture';
-import {GestureStatus} from '../gesture/type';
+//import {GestureStatus} from '../gesture/type';
 import {getTransformPropValue, setPxStyle, setTransform} from '../_utils/styleUtil';
 import DefaultTabBar from './DefaultTabBar';
 
@@ -187,7 +187,7 @@ export default class Tabs extends PureComponent {
     };
 
     return {
-      onPanStart: (status: GestureStatus) => {
+      onPanStart: status => {
         if (!this.props.swipeable || !this.props.animated) return;
         panDirection = getPanDirection(status.direction);
         this.setState({
@@ -195,7 +195,7 @@ export default class Tabs extends PureComponent {
         });
       },
 
-      onPanMove: (status: GestureStatus) => {
+      onPanMove: status => {
         const {swipeable, animated} = this.props;
         if (!status.moveStatus || !this.layout || !swipeable || !animated) return;
         const isVertical = this.isTabVertical();
