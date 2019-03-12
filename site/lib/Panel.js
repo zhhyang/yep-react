d032d2beb48233b0b79958925efe2d2413d535d8
zhhyang <zhhyang0404@163.com>
2019-03-12 15:40:34 +0800

    分类active

--- site/lib/Panel.js
+++ site/lib/Panel.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {toCamelCase} from './utils';
-
+import classNames from 'classnames'
 
 export default class Panel extends React.Component{
 
@@ -17,10 +17,11 @@ export default class Panel extends React.Component{
 
   render() {
     const {components,category,location} = this.props;
+    const isActive = Object.keys(components).includes(location.pathname.split('/').reverse()[0]) && components[location.pathname.split('/').reverse()[0]].category===category.name
     return (
       <li className={`nav-item${this.state.open ? ' nav-open' : ''}`}>
         <div className="nav-title" onClick={this.onClick} onKeyDown={() => {}} role="menu" tabIndex="-1">
-          <span>{category.label}</span>
+          <span className={classNames({'nav-title-active':isActive})}>{category.label}</span>
           <i />
         </div>
         <div className="nav-group">
