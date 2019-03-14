534ada26e82d5bded6aa09de5d66629353fc7b08
zhhyang <zhhyang0404@163.com>
2019-03-14 17:05:25 +0800

    demo构建支持img

--- scripts/webpack.demo.config.pro.js
+++ scripts/webpack.demo.config.pro.js
@@ -104,6 +104,17 @@ const config = {
           'sass-loader',
         ],
       },
+      {
+        // Exclude `js` files to keep "css" loader working as it injects
+        // its runtime that would otherwise be processed through "file" loader.
+        // Also exclude `html` and `json` extensions so they get processed
+        // by webpacks internal loaders.
+        exclude: [/\.(js|jsx|mjs|tsx)$/, /\.html$/, /\.json$/, /\.scss$/, /\.css$/],
+        loader: require.resolve('file-loader'),
+        options: {
+          name: '[name].[hash:8].[ext]',
+        },
+      },
     ],
   },
   optimization: {
