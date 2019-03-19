e270a5fb127a6dc6df13dbdaf1dd572988f9530c
wangkang12 <wangkang12@jd.com>
2019-03-19 11:29:49 +0800

    title

--- demo/Content.jsx
+++ demo/Content.jsx
@@ -2,48 +2,48 @@ import React, {Fragment} from 'react';
 import {Link, withRouter} from 'react-router-dom';
 import Prism from 'prismjs';
 import qs from 'qs';
-import {NavBar} from '@jdcfe/yep-react'
+import {NavBar} from '@jdcfe/yep-react';
 import allDocData from './allDocData';
 import Demo from './Demo';
+import ComponentTitle from './component/ComponentTitle';
 
-import {Helmet} from 'react-helmet'
-import {toCamelCase} from '../site/lib/utils'
-import App from "./App";
+import {Helmet} from 'react-helmet';
+import {toCamelCase} from '../site/lib/utils';
+import App from './App';
 
-const Content = ({ history, location: { pathname,search } }) => {
+const Content = ({history, location: {pathname, search}}) => {
   setTimeout(() => {
     Prism.highlightAll();
   }, 100);
-
 
   if (pathname.match(/\/component\//)) {
     const componentName = pathname.split('/').reverse()[0];
     const currentComponent = allDocData.components[componentName];
     if (!currentComponent) {
-      return (
-        <h1>404 page not found</h1>
-      );
+      return <h1>404 page not found</h1>;
     }
 
-    const query = qs.parse(search,{ ignoreQueryPrefix: true });
+    const query = qs.parse(search, {ignoreQueryPrefix: true});
     return (
       <Fragment>
-        <NavBar leftContent="返回" onLeftClick={() =>history.push('/')}>{toCamelCase(componentName)}</NavBar>
+        <NavBar leftContent="返回" onLeftClick={() => history.push('/')}>
+          YepUI
+        </NavBar>
         <div className="page-wrapper">
-          <Helmet title={toCamelCase(componentName)}/>
-          {
-            currentComponent && currentComponent.demos ? <Demo demo={currentComponent.demos.sort((a, b) => a.order - b.order)[query.order || 0]} componentName={componentName} />  : null
-          }
-          <style>
-            { currentComponent.style || '' }
-          </style>
+          <ComponentTitle title={query.title} englishTitle={toCamelCase(componentName)} />
+          <Helmet title={toCamelCase(componentName)} />
+          {currentComponent && currentComponent.demos ? (
+            <Demo
+              demo={currentComponent.demos.sort((a, b) => a.order - b.order)[query.order || 0]}
+              componentName={componentName}
+            />
+          ) : null}
+          <style>{currentComponent.style || ''}</style>
         </div>
       </Fragment>
     );
   }
-  return (
-    <App/>
-  );
+  return <App />;
 };
 
 export default withRouter(Content);
