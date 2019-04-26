0fdc49252b9eee0c1c13db725d87c2b76e32fbae
zhoujinxiu <zhoujinxiu@jd.com>
2019-04-26 14:08:30 +0800

    价格组件常规用法功能完成

--- src/price/index.tsx
+++ src/price/index.tsx
@@ -5,7 +5,10 @@ export interface PriceProps {
   prefixCls?: string;
   className?: string;
   style?: React.CSSProperties;
-  value?: string;
+  /**
+   * 价格
+   */
+  value: string;
   /**
    * 金币符号，默认为人民币符号¥。
    */
@@ -17,69 +20,94 @@ export interface PriceProps {
   /**
    * 小数部分展示的位数，0：不展示，1：展示1位，2：展示2位，默认展示2位。
    */
-  decimalNum?: number;
+  decLength?: number;
   /**
-   * 小数部分展示的位置，sub：右下，sup：右下，normal：正常，默认为右下。
+   * 小数部分展示的位置，sub：右下，sup：右上，normal：正常，默认为右下。
    */
   decPos?: string;
+  /**
+   * 删除状态，del：删除状态，默认为不删除即''
+   */
+  status?: string;
+  /**
+   * 符号额外样式
+   */
+  symClassName?: '';
+  /**
+   * 价格整数部分额外样式
+   */
+  intClassName?: '';
+  /**
+   * 价格小数部分额外样式
+   */
+  decClassName?: '';
 }
 
-export default class Price extends React.PureComponent<PriceProps, any> {
+const dealPrice = (value: string, decLength=2,symbol='¥') => {
+  // 判断value的第一位是否是货币符号
+  if (!/^[0-9]*$/.test(value.substring(0, 1))) {
+    symbol = value.substring(0, 1); // 如果是货币符号，获取符号
+    value = value.substring(1); // 获取价格
+  }
 
+  // 判断需要展示几位小数
+  value = parseFloat(value).toFixed(decLength);
+
+  const values = (value + '').split('.');
+
+  return {
+    symbol: symbol,
+    intVal: values[0],
+    decimalVal: values[1],
+  };
+};
+
+
+export default class Price extends React.PureComponent<PriceProps, any> {
   static defaultProps = {
     prefixCls: 'Yep-price',
     style: {},
     symbol: '¥',
     symPos: 'sub',
-    decimalNum: 2,
+    decLength: 2,
     decPos: 'sub',
-    value:'',
+    value: '36.00',
   };
 
   constructor(props: PriceProps) {
     super(props);
-    this.state = {
-      value:''
-    }
-  }
-
-  componentDidMount() {
-    let {value, decimalNum,symbol} = this.props;
-
-    // 判断value的第一位是否是货币符号
-    if (!/^[0-9]*$/.test(value.substring(0, 1))) {
-      symbol = value.substring(0, 1); // 如果是货币符号，获取符号
-      value = value.substring(1);
-    }
-
-    // 判断需要展示几位小数
-    if (decimalNum === 0) {
-      value = parseInt(value);
-    } else {
-      value = parseFloat(value).toFixed(decimalNum);
-    }
-    console.log(value,'value')
-    let values = (value+'').split('.');
-    this.setState({
-      symbol:symbol,
-      intVal:values[0],
-      decimalVal:values[1],
-    })
   }
 
   render() {
-    const {symbol,intVal,decimalVal} = this.state;
-    const {prefixCls,symPos} = this.props;
-    const cls = classNames({
-      'sym-sub':symPos==='sub',
-      'sym-sup':symPos==='sup',
+    let {prefixCls, symPos, decPos, status, className, symClassName, intClassName, decClassName, value, decLength, symbol} = this.props;
 
-    })
+    const cls = classNames({
+      'sym-sub': symPos === 'sub',
+      'sym-sup': symPos === 'sup',
+    });
+
+    const decCls = classNames({
+      'dec-sub': decPos === 'sub',
+      'dec-sup': decPos === 'sup',
+    });
+
+    const delCls = classNames({
+      'del': status === 'del',
+    });
+
+    symbol = dealPrice(value, decLength,symbol).symbol;
+    const intVal = dealPrice(value, decLength,symbol).intVal;
+    const decimalVal = dealPrice(value, decLength,symbol).decimalVal;
+
     return (
-      <div>
-        <span className={`${cls} ${prefixCls}-sym`}>{symbol}</span>
-        <span className={`${prefixCls}-txt int-txt`}>{intVal}</span>
-        <span className={`${prefixCls}-txt dec-txt`}>.{decimalVal}</span>
+      <div className={`${prefixCls} ${delCls} ${className ? `${className}` : ''}`}>
+        <span className={`${prefixCls}-sym ${cls} ${symClassName ? `${symClassName}` : ''}`}>{symbol}</span>
+        <span className={`${prefixCls}-txt int-txt ${intClassName ? `${intClassName}` : ''}`}>{intVal}</span>
+        {
+          !!decimalVal &&
+          <span
+            className={`${prefixCls}-txt dec-txt ${decCls} ${decClassName ? `${decClassName}` : ''}`}>.{decimalVal}</span>
+        }
       </div>
     );
   }
