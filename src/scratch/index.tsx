5e3b7ca491bedde3fb4d1994d8eb5373317f1cd7
zhhyang <zhhyang0404@163.com>
2019-12-09 15:04:06 +0800

    release:1.0.0-alpha.6

--- src/scratch/index.tsx
+++ src/scratch/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import classNames from 'classnames';
-
 export interface ScratchProps {
   prefixCls?: string;
   className?: string;
@@ -15,12 +14,11 @@ export interface State {
   isAreaShow: boolean;
   isArea2Show: boolean;
 }
-
 export default class Scratch extends React.PureComponent<ScratchProps, State> {
   static defaultProps = {
     prefixCls: 'Yep-scratch',
     style: {},
-    scratchImgUrl: '',
+    scratchImgUrl: 'http://img12.360buyimg.com/uba/jfs/t20377/335/194083123/261/85331804/5b02899bN0571f2a7.png',
     resCallback: () => {},
   };
 
@@ -38,70 +36,82 @@ export default class Scratch extends React.PureComponent<ScratchProps, State> {
   componentDidMount() {
     const wrap_canvas = document.getElementById('wrap_canvas') as HTMLDivElement;
     const icanvas = document.getElementById('myCanvas') as HTMLCanvasElement;
-    const iwidth = wrap_canvas && wrap_canvas.clientWidth;
-    const iheight = wrap_canvas && wrap_canvas.clientHeight;
+    // if (wrap_canvas && icanvas) {
+    const iwidth = wrap_canvas.clientWidth;
+    const iheight = wrap_canvas.clientHeight;
 
-    icanvas && (icanvas.width = iwidth);
-    icanvas && (icanvas.height = iheight);
+    icanvas.width = iwidth;
+    icanvas.height = iheight;
 
-    let context = icanvas && (icanvas.getContext('2d') as CanvasRenderingContext2D);
-    context && (context.lineCap = 'round');
-    context && (context.lineJoin = 'round');
+    // var drawMode = false;
 
+    let context = icanvas.getContext('2d') as CanvasRenderingContext2D;
+    context.lineCap = 'round';
+    context.lineJoin = 'round';
+    // context.fillStyle = 'rgb(193,193,193)';
+    // context.fillRect(0, 0, iwidth, iheight);
     let imgObj = new Image();
     imgObj.crossOrigin = 'Anonymous';
     imgObj.src = this.props.scratchImgUrl;
-
     //待图片加载完后，将其显示在canvas上
     imgObj.onload = function() {
-      //保持图片的原始大小：470*480
-      context.drawImage(imgObj, 0, 0, iwidth, iheight);
+      context.drawImage(imgObj, 0, 0, iwidth, iheight); //this即是imgObj,保持图片的原始大小：470*480
+      //ctx.drawImage(this, 0, 0,1024,768); //改变图片的大小到1024*768
 
       // 在源图像外显示目标图像。只有源图像外的目标图像部分会被显示，源图像是透明的。(源图像：刮痕区域，目标图像：带图片的初始canvas)
       context.globalCompositeOperation = 'destination-out';
     };
+    // context.font = '24px 微软雅黑';
+    // context.fillStyle = '#454444';
+    // var txt = '刮这里';
 
-    context && (context.lineWidth = 30);
+    // context.fillText(txt, iwidth / 2 - context.measureText(txt).width / 2, iheight / 2 + 9);
+    // context.globalCompositeOperation = 'destination-out';
+    context.lineWidth = 30;
+    // }
   }
 
   touchStart(e: any) {
-    let icanvas = document.getElementById('myCanvas') as HTMLCanvasElement;
-    let context = icanvas.getContext('2d') as CanvasRenderingContext2D;
+    var icanvas = document.getElementById('myCanvas') as HTMLCanvasElement;
+    var context = icanvas.getContext('2d') as CanvasRenderingContext2D;
     context.beginPath();
-    let rect = icanvas.getBoundingClientRect();
+    var rect = icanvas.getBoundingClientRect();
     context.moveTo(e.changedTouches[0].clientX - rect.left, e.changedTouches[0].clientY - rect.top);
+    // $("#area2").show();
   }
 
   touchMove(e: any) {
+    // $icanvas.off('touchmove');
+    e.persist();
     this.setState({
       isArea2Show: true,
     });
-
+    // var e = event;
     const wrap_canvas = document.getElementById('wrap_canvas') as HTMLDivElement;
-    let icanvas = document.getElementById('myCanvas') as HTMLCanvasElement;
-    let context = icanvas && (icanvas.getContext('2d') as CanvasRenderingContext2D);
-    let rect = icanvas && icanvas.getBoundingClientRect();
+    var icanvas = document.getElementById('myCanvas') as HTMLCanvasElement;
+    var context = icanvas.getContext('2d') as CanvasRenderingContext2D;
 
-    context && context.lineTo(e.changedTouches[0].clientX - rect.left, e.changedTouches[0].clientY - rect.top);
-    context && context.stroke();
+    // if (this.state.down) {
+    var rect = icanvas.getBoundingClientRect();
 
-    let iwidth = wrap_canvas && wrap_canvas.clientWidth,
-      iheight = wrap_canvas && wrap_canvas.clientHeight,
-      imgData = context && context.getImageData(0, 0, iwidth, iheight),
-      pixles = imgData && imgData.data,
+    context.lineTo(e.changedTouches[0].clientX - rect.left, e.changedTouches[0].clientY - rect.top);
+    context.stroke();
+    // }
+    var iwidth = wrap_canvas.clientWidth;
+    var iheight = wrap_canvas.clientHeight;
+    var imgData = context.getImageData(0, 0, iwidth, iheight),
+      pixles = imgData.data,
       transPixs = [];
-
-    for (let i = 0, j = pixles && pixles.length; i < j; i += 4) {
-      let a = pixles[i + 3];
+    for (var i = 0, j = pixles.length; i < j; i += 4) {
+      var a = pixles[i + 3];
       if (a < 128) {
         //@ts-ignore
         transPixs.push(i);
       }
     }
-
     //@ts-ignore
     if (
-      ((transPixs.length / (pixles && pixles.length / 4)) * 100).toFixed(2) >
+      ((transPixs.length / (pixles.length / 4)) * 100).toFixed(2) >
       (this.props.resPercent >= 0 ? this.props.resPercent : 60)
     ) {
       this.props.resCallback();
@@ -109,10 +119,18 @@ export default class Scratch extends React.PureComponent<ScratchProps, State> {
         isAreaShow: false,
       });
     }
+    e.preventDefault && e.preventDefault();
+    e.returnValue = false;
+    e.stopPropagation && e.stopPropagation();
     return false;
   }
 
-  touchEnd() {}
+  touchEnd() {
+    // $icanvas.off('touchend');
+    // this.setState({
+    //   down: false,
+    // });
+  }
 
   render() {
     const {prefixCls, className, style} = this.props;
